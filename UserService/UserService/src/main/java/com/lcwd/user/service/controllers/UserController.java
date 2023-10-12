34f2fc1ac37e9a6592dc3134a661183ab249065a
Dhruvilshah007 <sdhruvil72@gmail.com>
2023-10-13 00:15:59 +0530

    Implementing Circuit Breaker using Resilience4J

--- UserService/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.lcwd.user.service.controllers;
 
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
+import com.lcwd.user.service.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserService userService;
@@ -31,10 +37,25 @@ public class UserController {
     //single user get
 
     @GetMapping("{userId}")
+    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user= userService.getUser(userId);
         return ResponseEntity.ok(user);
 
+    }
+
+    //creating fall back method for circuit breaker.This will only be called if any(rating/hotel) service is DOWN
+    //IMP point - Return type and parameters(should match) of fallback method and main method should be same.For ge in our case ResponseEntity<User>
+    //For configuration we can do Java based as well as Resilence4j(yml based configuration)
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+        logger.info("Fallback is executed because service is down->"+ex.getMessage());
+        User user=User.builder()
+                .email("dummy@gmail.com")
+                .name("dummy")
+                .about("this user is created dummy because some service ism down")
+                .userId("135235")
+                .build();
+        return  new ResponseEntity<>(user,HttpStatus.OK);
     }
 
 
