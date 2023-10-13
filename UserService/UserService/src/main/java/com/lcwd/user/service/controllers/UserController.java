9687c56ab02aa8c549a49a1c65f56782a12a05e6
Dhruvilshah007 <sdhruvil72@gmail.com>
2023-10-14 02:01:16 +0530

    Implemented RETRY

--- UserService/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -4,7 +4,7 @@ package com.lcwd.user.service.controllers;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
 import com.lcwd.user.service.services.impl.UserServiceImpl;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +18,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserService userService;
@@ -34,21 +34,29 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
+    int retryCount=1;
     //single user get
 
     @GetMapping("{userId}")
-    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+//    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    @Retry(name ="ratingHotelService",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        logger.info("UserController->getSingleUser starts");
+        logger.info("Retry Count:{}",retryCount);
+        retryCount++;
         User user= userService.getUser(userId);
         return ResponseEntity.ok(user);
 
     }
 
+
+
     //creating fall back method for circuit breaker.This will only be called if any(rating/hotel) service is DOWN
     //IMP point - Return type and parameters(should match) of fallback method and main method should be same.For ge in our case ResponseEntity<User>
     //For configuration we can do Java based as well as Resilence4j(yml based configuration)
     public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
-        logger.info("Fallback is executed because service is down->"+ex.getMessage());
+//        logger.info("Fallback is executed because service is down->"+ex.getMessage());
+
         User user=User.builder()
                 .email("dummy@gmail.com")
                 .name("dummy")
