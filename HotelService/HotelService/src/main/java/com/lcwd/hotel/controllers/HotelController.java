b5370ef126dc12859b7f3033739f7d7081febe27
Dhruvilshah007 <sdhruvil72@gmail.com>
2023-10-21 22:21:10 +0530

    Implemented Spring security and OKTA in HotelService and RatingService.Authorized Hotel and Rating Controller

--- HotelService/HotelService/src/main/java/com/lcwd/hotel/controllers/HotelController.java
+++ HotelService/HotelService/src/main/java/com/lcwd/hotel/controllers/HotelController.java
@@ -6,6 +6,7 @@ import com.lcwd.hotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,15 +17,18 @@ public class HotelController {
     @Autowired
     private HotelService hotelService;
     //create
+    // Hotel creation can be done by only user with ADMIN role
 
-
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
     }
 
-    //single user
+    //get hotel info by id
+    //No one will be able to R/Q data directly, as it can be only called via Microservices internally as per scope
 
+    @PreAuthorize("hasAuthority('SCOPE_INTERNAL')")
     @GetMapping("{hotelId}")
     public ResponseEntity<Hotel> get(@PathVariable String hotelId){
         return ResponseEntity.ok(hotelService.get(hotelId));
@@ -32,7 +36,10 @@ public class HotelController {
     }
 
 
-    //all user get
+    //all hotel get
+    //No one will be able to R/Q data directly, as it can be only called via Microservices internally as per scope and User with Admin rights
+
+    @PreAuthorize("hasAuthority('SCOPE_INTERNAL') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Hotel>> getAll(){
         return ResponseEntity.ok(hotelService.getAll());
--- Rating service/Rating-service/src/main/java/com/lcwd/rating/controller/RatingController.java
+++ Rating service/Rating-service/src/main/java/com/lcwd/rating/controller/RatingController.java
@@ -6,6 +6,7 @@ import com.lcwd.rating.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,9 +17,9 @@ public class RatingController {
     @Autowired
     private RatingService ratingService;
 
-    //creeate
-
-
+    //create
+    // Rating creation can be done by only user with ADMIN role
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Rating> create(@RequestBody Rating rating){
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
@@ -34,6 +35,9 @@ public class RatingController {
 
 
     //Get all rating of user
+
+
+    @PreAuthorize("hasAuthority('SCOPE_INTERNAL') || hasAuthority('Admin')")
     @GetMapping("/users/{userId}")
     public ResponseEntity<List<Rating>> getRatingByUserId(@PathVariable String userId){
         return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
